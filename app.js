14be8e98649ca827caa6f10c1993abac4e63ba16
asigitj <asigitj1@gmail.com>
2022-10-25 01:17:19 +0700

    memperbaiki cors

--- app.js
+++ app.js
@@ -59,6 +59,8 @@ app.use(`${URL}/auth`, authRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
 
