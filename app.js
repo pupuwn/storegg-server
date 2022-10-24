b33651c00e95ccc26d8f7f9c166942783610040a
asigitj <asigitj1@gmail.com>
2022-10-25 01:22:11 +0700

    .env

--- app.js
+++ app.js
@@ -58,9 +58,6 @@ app.use(`${URL}/players`, playerRouter);
 app.use(`${URL}/auth`, authRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
 
